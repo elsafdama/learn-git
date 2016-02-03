515341e3b66212d76e5f72ef077933cd27063cc3
Elsafitri Damayanti <elsafitri.dama@gmail.com>
2016-02-03 13:07:48 +0700

    commit kedua

---
+++ variable.rb
@@ -0,0 +1,27 @@
+#string
+nama = "elsa";
+puts (nama);
+
+puts nama;
+
+#string replacement
+puts "Halo #{nama}";
+puts "Halo" +nama; 
+puts "2+1 = #{2+1}";
+
+#boolean
+
+pria = true;
+puts "Apakah dia pria? #{pria}";
+puts "Apakah dia bukan pria? #{!pria}"; 
+
+dewasa = false;
+puts "Apakah dia pria dewasa? #{pria && dewasa}";
+    
+#number
+umur = 20;
+puts "umur #{nama} #{umur} tahun";
+puts "2 tahun lagi umur #{nama} , #{umur +2} tahun";
+
+nama_belakang = "damayanti";
+puts "Nama Lengkap = #{nama + nama_belakang}";
