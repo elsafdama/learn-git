bbea5ba4cfa2c95626799dfb892a4c6f8e7c65e6
Elsafitri Damayanti <elsafitri.dama@gmail.com>
2016-02-03 13:15:51 +0700

    commit ketiga

---
+++ operator.rb
@@ -0,0 +1,10 @@
+num1 =5;
+num2 =3;
+
+puts "num1 + num2 = #{num1 + num2}"
+puts "num1 ** num2 = #{num1 ** num2}"
+puts "num1 == num2 = #{num1 == num2}"
+puts "num1 / num2 = #{Float(num1 / num2)}"
+puts "num1 + num2 = #{num1 + num2}"
+puts "num1 / num2 = #{(num1 / num2).to_f}"
+
